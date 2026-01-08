f415637de99f87a864bc2e507729102d323f7136
hanq11 <>
2026-01-08 13:43:17 +0700

    update

---
+++ sd20308jav201/src/main/java/com/example/sd20308jav201/buoi2/model/TruongHoc.java
@@ -0,0 +1,25 @@
+package com.example.sd20308jav201.buoi2.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "truong_hoc")
+@Entity
+public class TruongHoc {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "ten_truong")
+    private String tenTruong;
+
+    @Column(name = "dia_chi")
+    private String diaChi;
+}
